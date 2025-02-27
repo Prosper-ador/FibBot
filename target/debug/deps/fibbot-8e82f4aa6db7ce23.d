8fdcbf7189ab2cf9faee4da37089b7616518050a
prosper-ador <prosnyengka@gmail.com>
2025-02-27 17:09:52 +0100

    added target

---
+++ target/debug/deps/fibbot-8e82f4aa6db7ce23.d
@@ -0,0 +1,5 @@
+/home/nyengka-prosper/projects/rust_projects/fibbot/target/debug/deps/fibbot-8e82f4aa6db7ce23: src/main.rs
+
+/home/nyengka-prosper/projects/rust_projects/fibbot/target/debug/deps/fibbot-8e82f4aa6db7ce23.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/fibbot.d
@@ -0,0 +1,1 @@
+/home/nyengka-prosper/projects/rust_projects/fibbot/target/debug/fibbot: /home/nyengka-prosper/projects/rust_projects/fibbot/src/main.rs
